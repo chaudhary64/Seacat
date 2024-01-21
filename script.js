93c3273dec594390e7ff4e332ebe0d7d21e5129e
chaudhary64 <chiragchoudhary64@gmail.com>
2024-01-21 20:10:30 +0530

    Its Almost completed

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-let v1 = document.querySelector('.video1')
-let v2 = document.querySelector('.video2')
+let v1 = document.querySelector(".video1");
+let v2 = document.querySelector(".video2");
+let lights = document.querySelector(".lights");
+let lc = document.querySelector(".Light_Container");
+let slider = document.querySelector(".Slider");
+let dots = document.querySelector(".Dots");
+
+
+Draggable.create(".lights", {
+  type: "x",
+  bounds: lc,
+  inertia: true,
+  onDragEnd: function () {
+    let lightscoordinate = lights.getBoundingClientRect();
+    console.log(lightscoordinate.left);
+    if(lightscoordinate.left == 883.75) {
+        v2.style.opacity = 1;
+        console.log('yaay')
+    }
+    else {
+        v2.style.opacity = 0;
+        console.log('naay')
+    }
+  },
+});
+
+
 
